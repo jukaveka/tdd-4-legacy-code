34493984b385967f21685f8bf783bd4d7d932438
jukaveka <juhokavekari@hotmail.com>
2026-05-09 22:52:55 +0300

    Test backstage pass item for coverage

--- test/gilded_rose.test.mjs
+++ test/gilded_rose.test.mjs
@@ -8,4 +8,10 @@ describe("Gilded Rose", () => {
     const items = gildedRose.updateQuality();
     expect(items[0].name).to.equal("foo");
   });
+
+  test("backstage pass", () => {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).to.equal("Backstage passes to a TAFKAL80ETC concert");
+  });
 });
