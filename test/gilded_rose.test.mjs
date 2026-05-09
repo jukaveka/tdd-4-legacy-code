194ba2916c5ce0b60bff7348ec7956984f318767
jukaveka <juhokavekari@hotmail.com>
2026-05-10 00:33:26 +0300

    Test aged brie for coverage

--- test/gilded_rose.test.mjs
+++ test/gilded_rose.test.mjs
@@ -21,12 +21,6 @@ describe("Gilded Rose", () => {
     expect(items[0].sellIn).to.equal(0);
   });
 
-  test("backstage pass", () => {
-    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).to.equal("Backstage passes to a TAFKAL80ETC concert");
-  });
-
   test("backstage pass increases with sellIn date left", () => {
     const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 10, 10)]);
     const items = gildedRose.updateQuality();
@@ -40,8 +34,8 @@ describe("Gilded Rose", () => {
   });
 
   test("aged brie", () => {
-    const gildedRose = new Shop([new Item("aged brie", 0, 0)]);
+    const gildedRose = new Shop([new Item("aged brie", 0, 10)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).to.equal("aged brie");
+    expect(items[0].quality).to.equal(8);
   });
 });
