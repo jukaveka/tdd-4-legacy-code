375d5f75e0ceeac3afcbeb73f43afcfc50b16ebb
jukaveka <juhokavekari@hotmail.com>
2026-05-09 23:11:18 +0300

    Test backstage pass sellIn date

--- test/gilded_rose.test.mjs
+++ test/gilded_rose.test.mjs
@@ -27,6 +27,18 @@ describe("Gilded Rose", () => {
     expect(items[0].name).to.equal("Backstage passes to a TAFKAL80ETC concert");
   });
 
+  test("backstage pass increases with sellIn date left", () => {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 10, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(12);
+  });
+
+  test("backstage pass loses value past sellIn date", () => {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 0, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(0);
+  });
+
   test("aged brie", () => {
     const gildedRose = new Shop([new Item("aged brie", 0, 0)]);
     const items = gildedRose.updateQuality();
