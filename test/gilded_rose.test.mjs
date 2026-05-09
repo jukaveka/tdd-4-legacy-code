7c87138f4066a93439a3889f429ba18d4cfc76c6
jukaveka <juhokavekari@hotmail.com>
2026-05-09 23:03:47 +0300

    Test quality decreasing by one for test coverage

--- test/gilded_rose.test.mjs
+++ test/gilded_rose.test.mjs
@@ -9,6 +9,12 @@ describe("Gilded Rose", () => {
     expect(items[0].name).to.equal("foo");
   });
 
+  test("quality 1", () => {
+    const gildedRose = new Shop([new Item("foo", 0, 1)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(0);
+  });
+
   test("backstage pass", () => {
     const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 0, 0)]);
     const items = gildedRose.updateQuality();
