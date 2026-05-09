47381c7c5d30162b24dcda0058c269c01caadc5d
jukaveka <juhokavekari@hotmail.com>
2026-05-09 23:05:26 +0300

    Test sellIn 1 for coverage

--- test/gilded_rose.test.mjs
+++ test/gilded_rose.test.mjs
@@ -15,6 +15,12 @@ describe("Gilded Rose", () => {
     expect(items[0].quality).to.equal(0);
   });
 
+  test("sellIn 1", () => {
+    const gildedRose = new Shop([new Item("foo", 1, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).to.equal(0);
+  });
+
   test("backstage pass", () => {
     const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 0, 0)]);
     const items = gildedRose.updateQuality();
@@ -25,11 +31,5 @@ describe("Gilded Rose", () => {
     const gildedRose = new Shop([new Item("aged brie", 0, 0)]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).to.equal("aged brie");
-  });
-
-  test("aged brie & sellIn < 0", () => {
-    const gildedRose = new Shop([new Item("aged brie", 1, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].sellIn).to.equal(0);
   });
 });
